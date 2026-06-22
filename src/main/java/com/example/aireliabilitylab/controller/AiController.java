ff642ecacbfe8f5b767dd1a2cdf2b433c2f82cc1
JonatanDahl <jdahl.junior@gmail.com>
2026-06-22 23:03:11 +0200

    Added POST endpoint for sentiment analysis in AiController

--- src/main/java/com/example/aireliabilitylab/controller/AiController.java
+++ src/main/java/com/example/aireliabilitylab/controller/AiController.java
@@ -1,5 +1,6 @@
 package com.example.aireliabilitylab.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,8 @@ public class AiController {
         this.aiClientService = aiClientService;
     }
 
+    @PostMapping("/sentiment")
     public AiResponseDto analyzeSentiment(@Valid @RequestBody AiRequestDto request) {
         return aiClientService.analyzeSentiment(request.getText());
     }
-
-
-
-
-    
 }
