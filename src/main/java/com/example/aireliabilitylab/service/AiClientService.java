2653614fe5b1e4360292c0ff9f3188e4898aa433
JonatanDahl <jdahl.junior@gmail.com>
2026-06-23 21:35:18 +0200

    Changed AiClientService with API key configuration and RestClient setup for sentiment analysis

--- src/main/java/com/example/aireliabilitylab/service/AiClientService.java
+++ src/main/java/com/example/aireliabilitylab/service/AiClientService.java
@@ -1,15 +1,39 @@
 package com.example.aireliabilitylab.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-
-import com.example.aireliabilitylab.dto.AiResponseDto;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class AiClientService {
     
-        public AiResponseDto analyzeSentiment(String text) {
+    @Value("${ai.api.key}")
+    private String apiKey;
 
-            return new AiResponseDto("posetive", 90);
-        }
+    private final RestClient restClient;
+
+    private static final String SYSTEM_PROMPT = 
+    "You are a helpful assistant that analyzes the sentiment of the given text. Respond with a JSON object containing the sentiment (positive, negative, or neutral) and a brief explanation.";
+
+    public AiClientService(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(2000); // 2 seconds
+        factory.setReadTimeout(8000); // 8 seconds
+        this.restClient = RestClient.builder()
+                .requestFactory(factory)
+                .baseUrl("https://api.openai.com/v1")
+                .build();
+    }
+
+
+
+
+
+    
+
+
+
+
     
 }
